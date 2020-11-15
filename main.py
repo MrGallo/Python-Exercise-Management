39c26e9e0d99d10ec1bc2cdac2ea2b3e4d5173d6
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2020-11-15 00:28:21 +0000

    Rermove issues

--- main.py
+++ main.py
@@ -2,10 +2,3 @@ import management
 
 
 management.main()
-
-
-"""
-- Write whole exercise to md file (tests and such)
-- Need a better url for the repo. currently:
-  https://mrgallo.github.io/Python-Exercise-Management
-"""
